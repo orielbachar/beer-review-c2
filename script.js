a3e71535eef98226ed09e557a0ddf101c4287080
Oriel Bachar <orielbachar@Oriels-MacBook-Pro.local>
2016-11-13 17:13:19 +0200

    push

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 //Beer-Rewview-Project
-function beer(name, category, rate){
-  this.name = name;
-  this.category = category;
-  this.rate = rate;
+
+var BeerReviewApp = function(){
+  var beers = [];
+  function beer(name, category, rate){
+    this.name = name;
+    this.category = category;
+    this.rate = rate;
+  };
+
+  var addBeer = function(name, category, rate){
+
+    var beer1 = new beer(name, category, rate);
+    beers.push(beer1);
+    $('.beer-input').val('');
+    $('.category-input').val('');
+
+  };
+
+  var renderBeers = function(){
+    $('.beers-list').find('li').remove();
+    for (var i = 0; i<beers.length; i++){
+      $('.beers-list').append('<li>' + beers[i].name + " "  + beers[i].category + " " + beers[i].rate + '</li>');
+    }
+  };
+
+  return {
+    addBeer: addBeer,
+    renderBeers: renderBeers
+  };
 };
 
-var beers = [];
-
-var addBeer = function(name, category, rate){
-
-  var beer1 = new beer(name, category, rate);
-  beers.push(beer1);
-  $('.beer-input').val('');
-  $('.category-input').val('');
-  $('.select-input').val('');
-
-};
-
-var updateBeers = function(){
-  $('.beers-list').find('li').remove();
-  for (var i = 0; i<beers.length; i++){
-    $('.beers-list').append('<li>' + beers[i].name + " "  + beers[i].category + " " + beers[i].rate + '</li>');
-  }
-};
+  var app = BeerReviewApp();
 
 $('.post-beer').click(function(){
-  addBeer($('.beer-input').val(), $('.category-input').val(), $('.select-input').val());
-  updateBeers();
+  app.addBeer($('.beer-input').val(), $('.category-input').val(), $('.select-input').val());
+  app.renderBeers();
 });
-
-$('.select-beer').click(function(){
-
-
-}
